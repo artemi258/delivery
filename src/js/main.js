4d41379ee2e8760f85fbecab0c37d8900315e6a2
It city <It@mail.ru>
2021-10-11 18:19:59 +0500

    work1110

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import slider from './modules/slider';
+import { WOW } from 'wowjs';
+new WOW().init();
 
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strick';
 
-
+   
    slider();
         
 });
